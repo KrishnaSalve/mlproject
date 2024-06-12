7c7fd09ddb5c1d264260ca62318166a94d92ee9c
Krishna Salve <krishnasalve97@gmail.com>
2024-06-12 10:45:31 +0530

    Adding files

--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -7,6 +7,8 @@ import numpy as np
 from dotenv import load_dotenv
 import pymysql
 
+import pickle
+
 load_dotenv()
 
 host=os.getenv("host")
@@ -36,3 +38,14 @@ def read_sql_data():
 
     except Exception as ex:
         raise CustomException(ex)
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok = True)
+
+        with open(file_path, 'wb') as file_obj:
+            pickle.dump(obj, file_obj)
+            
+    except Exception as e:
+        raise CustomException(e,sys)
